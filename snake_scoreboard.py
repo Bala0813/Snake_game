13305dc02a746d2b925d2d4ae72d7429fc1e2dfa
BalaKumar <131630250+Bala0813@users.noreply.github.com>
2023-08-09 17:16:37 -0600

    snake_scoreboard.py

--- snake_scoreboard.py
+++ snake_scoreboard.py
@@ -4,6 +4,7 @@ class FoodScore(Turtle):
     def __init__(self):
         super().__init__()
         self.snake_score = 0
+        self.high_score = 0
         self.color("white")
         self.hideturtle()
         self.penup()
@@ -17,8 +18,16 @@ class FoodScore(Turtle):
 
     def game_over(self):
         self.goto(0, 0)
+        with open("score.txt", "r") as score_sheet:
+            self.high_score = int(score_sheet.read())
+            dum = self.high_score
+            if self.snake_score > dum:
+                self.high_score = self.snake_score
+                with open("score.txt", "w") as score_sheet:
+                    score_sheet.write(str(self.high_score))
         self.write("Game Over!", move=False, align="center", font=("arial", 30, "normal"))
 
     def score_writer(self):
-        self.write(f"Score: {self.snake_score}", move=False, align="center", font=("arial", 30, "normal"))
-
+        with open("score.txt", "r") as score_sheet:
+            self.high_score = score_sheet.read()
+        self.write(f"High Score: {self.high_score}\n Score: {self.snake_score}", move=False, align="center", font=("arial", 30, "normal"))
